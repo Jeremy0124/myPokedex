2840bb1d075502539004d72833e2cf97769b0c48
Jeremy0124 <Jeremy.0124@yahoo.com>
2022-07-12 19:35:03 -0600

    Initial Commit

---
+++ myPokedex/myPokedexApp.swift
@@ -0,0 +1,17 @@
+//
+//  myPokedexApp.swift
+//  myPokedex
+//
+//  Created by Jeremy Jackman on 7/12/22.
+//
+
+import SwiftUI
+
+@main
+struct myPokedexApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
